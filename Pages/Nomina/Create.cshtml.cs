9720111f760830cf0553d6fa50c4d83171febb20
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-23 04:24:27 -0500

    create nomina

---
+++ Pages/Nomina/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using GestionRecursosHumanos2.Data;
+using GestionRecursosHumanos2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionRecursosHumanos2.Pages.Nomina
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GestionRecursosContext _context;
+
+        public CreateModel(GestionRecursosContext context)
+        {
+            this._context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+
+        [BindProperty]
+        public Nominas Nomina { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Nomina == null || Nomina == null)
+            {
+                return Page();
+            }
+
+            _context.Nomina.Add(Nomina);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
