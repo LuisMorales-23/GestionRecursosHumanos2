890fa603d5dea7a75441bf56ec9b9012cf413d5c
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-23 03:59:07 -0500

    edit y delete de empleados

---
+++ Pages/Empleado/Edit.cshtml.cs
@@ -0,0 +1,69 @@
+using GestionRecursosHumanos2.Data;
+using GestionRecursosHumanos2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionRecursosHumanos2.Pages.Empleado
+{
+    public class EditModel : PageModel
+    {
+        private readonly GestionRecursosContext _context;
+
+        public EditModel(GestionRecursosContext context)
+        {
+            this._context = context;
+        }
+        [BindProperty]
+        public Empleados Empleado { get; set; }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleado.FirstOrDefaultAsync(m =>
+                m.IdEmpleados == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            Empleado = empleado;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Attach(Empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(Empleado.IdEmpleados))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToPage("./Index");
+        }
+
+        public bool EmpleadoExists(int id)
+        {
+            return (_context.Empleado?.Any(e => e.IdEmpleados == id)).GetValueOrDefault();
+        }
+
+        ///----///
+    }
+}
