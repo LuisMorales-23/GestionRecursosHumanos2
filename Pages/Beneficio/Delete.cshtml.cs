0de54236df5a531f163835bfaba74d0c45940c92
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-23 04:10:31 -0500

    delete de beneficios

---
+++ Pages/Beneficio/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+using GestionRecursosHumanos2.Data;
+using GestionRecursosHumanos2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionRecursosHumanos2.Pages.Beneficio
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly GestionRecursosContext _context;
+
+        public DeleteModel(GestionRecursosContext context)
+        {
+            this._context = context;
+        }
+
+        [BindProperty]
+        public Beneficios Beneficio { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Beneficio == null)
+            {
+                return NotFound();
+            }
+
+            var beneficio = await _context.Beneficio.FirstOrDefaultAsync(m => m.IdBeneficio == id);
+
+            if (beneficio == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Beneficio = beneficio;
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Beneficio == null)
+            {
+                return NotFound();
+            }
+            var beneficio = await _context.Beneficio.FindAsync(id);
+
+            if (beneficio != null)
+            {
+                Beneficio = beneficio;
+                _context.Beneficio.Remove(Beneficio);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        ///
+    }
+}
